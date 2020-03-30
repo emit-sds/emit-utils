6b0a7a06298f1e4de3837fb2d720bcbe48d870fc
Brodrick <philip.brodrick@jpl.nasa.gov>
2020-03-30 14:32:09 -0700

    added file for common logging calls

---
+++ emit_utils/common_logs.py
@@ -0,0 +1,21 @@
+"""
+This code holds some common logging calls, useful to standardize format.
+
+Author: Philip G. Brodrick, philip.brodrick@jpl.nasa.gov
+"""
+
+import time
+import logging
+
+def logtime():
+    """
+    Log the current UTC time.
+    Args:
+        None
+    Returns:
+        None
+    """
+
+    start_time = time.strftime('%Y%m%d_%H%M%S', time.gmtime())
+
+    logging.log('Current UTC time is: {}'.format(start_time))
