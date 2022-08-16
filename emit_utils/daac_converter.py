342ee948c71a133250fdddb39d3426fd571d9a96
Winston Olson-Duvall <winston.olson-duvall@jpl.nasa.gov>
2022-08-16 12:47:13 -0700

    Read in points as floats.

--- emit_utils/daac_converter.py
+++ emit_utils/daac_converter.py
@@ -408,7 +408,7 @@ def get_gring_boundary_points(glt_hdr_path: str):
     gring = hdr["gring"]
     points = []
     for i in range(1, len(gring), 2):
-        points.append([gring[i], gring[i + 1]])
+        points.append([float(gring[i]), float(gring[i + 1])])
     return points
 
 
