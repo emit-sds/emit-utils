3596959d5f1b103e82179361ffd4d5ed3b0a7ab2
Winston Olson-Duvall <winston.olson-duvall@jpl.nasa.gov>
2023-08-18 15:19:09 -0700

    Fix runconfig references

--- emit_utils/daac_converter.py
+++ emit_utils/daac_converter.py
@@ -240,9 +240,9 @@ def makeGlobalAttr(nc_ds: netCDF4.Dataset, primary_envi_file: str, software_deli
     if rdn_runconfig_file is not None:
         with open(rdn_runconfig_file, "r") as f:
             runconfig = json.load(f)
-    ffupdate_files = [os.path.basename(p) for p in runconfig["flat_field_update_paths"]]
-    ffupdate_str = ",".join(ffupdate_files)
-    primary_ds.metadata['emit pge input files'].append(f"ffupdate_files=[{ffupdate_str}]")
+        ffupdate_files = [os.path.basename(p) for p in runconfig["flat_field_update_paths"]]
+        ffupdate_str = ",".join(ffupdate_files)
+        primary_ds.metadata['emit pge input files'].append(f"ffupdate_files=[{ffupdate_str}]")
     run_command = "PGE Run Command: {" + primary_ds.metadata['emit pge run command'] + "}"
     input_files = "PGE Input Files: {" + ", ".join(primary_ds.metadata['emit pge input files']) + "}"
     nc_ds.history = run_command + ", " + input_files
