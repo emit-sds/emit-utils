2e1ae8ec39597505dfa9aae5f746f845a93efe40
Winston Olson-Duvall <winston.olson-duvall@jpl.nasa.gov>
2026-09-02 21:50:45 -0700

    Remove crosstrack_orientation

--- emit_utils/daac_converter.py
+++ emit_utils/daac_converter.py
@@ -260,11 +260,11 @@ def makeGlobalAttr(nc_ds: netCDF4.Dataset, primary_envi_file: str, software_deli
     run_command = "PGE Run Command: {" + primary_ds.metadata['emit pge run command'] + "}"
     input_files = "PGE Input Files: {" + ", ".join(primary_ds.metadata['emit pge input files']) + "}"
     nc_ds.history = run_command + ", " + input_files
-    if 'flip horizontal' in primary_ds.metadata.keys():
-        if int(primary_ds.metadata['flip horizontal']) == 1:
-            nc_ds.crosstrack_orientation = 'as seen on ground'
-        else:
-            nc_ds.crosstrack_orientation = 'as seen by fpa'
+    # if 'flip horizontal' in primary_ds.metadata.keys():
+    #     if int(primary_ds.metadata['flip horizontal']) == 1:
+    #         nc_ds.crosstrack_orientation = 'as seen on ground'
+    #     else:
+    #         nc_ds.crosstrack_orientation = 'as seen by fpa'
 
     # only include spatial information if provided (may not be available for all PGEs)
     if glt_envi_file is not None:
